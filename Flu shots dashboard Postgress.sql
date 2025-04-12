64c7fbbccd21aa3170e67b49012de36f936253eb
Caroline Kibunja <carolinekibunja@gmail.com>
2025-04-13 00:59:02 +0300

    Add files via upload

---
+++ Flu shots dashboard Postgress.sql
@@ -0,0 +1,15 @@
+/*
+Objectives
+Come up with flu shots dashboard for 2022 that does the following 
+
+1.)Total % of patients getting flu shots stratified by;
+	a.)Age
+	b.)Race
+	c.)County on a map
+	d.)Overall
+2.) Running Total of flu shots over the course of 2022
+3.) Total number of flu shots given in 2022
+4.) A list of patients that show whether or not they received the flu shots
+
+Requirements 
+Patients must have been active at our hospital 
